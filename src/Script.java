bc8202743197cc66f883472fc6ee0fba4c9f405c
EslamElkhafagy <eslamelkhafagy@gmail.com>
2020-05-01 05:29:39 +0200

    edit in pom.xml and jenkins file

--- src/Script.java
+++ src/Script.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,180 +11,321 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
 
+import javax.xml.parsers.*;
+
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class Script {
 	static ArrayList<File> files = new ArrayList<>();
-	static byte current=0; // to know current method is get all branches
-	static ArrayList<String> branchesName= new ArrayList<>();
-	static String path="//media//elkhafagy//Elkhafagy//TestJenkins//AskFM";//
+	static byte current = 0; // to know current method is get all branches
+	static ArrayList<String> branchesName = new ArrayList<>();
+	static String path = "//media//elkhafagy//Elkhafagy//TestJenkins//AskFM";//
 
+	public static final String jenkinsPath = "/media/elkhafagy/Elkhafagy/Elkhafagy/special/VFES-DXL-Fault-Management/Jenkinsfile";
 
+	static void modifyFile(String filePath, String branchVersion) {
+		File fileToBeModified = new File(filePath);
 
-		public static void main(String[] args) throws IOException, InterruptedException {
-	  
-		runJenkinsScript();	
-		
-		
-	      }
-	     
-	
-	
-	     // Uses Files.walk method
-	     public static void listAllFiles(String path){
-	         System.out.println("In listAllfiles(String path) method");
-	         try(
-	        		 Stream<Path> paths = Files.walk(Paths.get(path))) {
-	             paths.forEach(filePath -> {
-	                 if (Files.isRegularFile(filePath)) {
-	                     try {
-	                         files.add(new File(filePath.toString()));
-	                     } catch (Exception e) {
-	                         // TODO Auto-generated catch block
-	                         e.printStackTrace();
-	                     }
-	                 }
-	             });
-	             renameJenkinsFile();
-	         } catch (IOException e) {
-	             // TODO Auto-generated catch block
-	             e.printStackTrace();
-	         }
-	     }
-	     
-	     
-	     public static void renameJenkinsFile() {
-	    	 int count=0;
-	    	 for (int i = 0; i < files.size(); i ++){
-	    		 if(files.get(i).getName().startsWith("jenkinsfile"))
-	    	        files.get(i).renameTo(new File(files.get(i).getPath() + "8787"));
-//	    	 System.out.println(files.get(i).getPath());
-	    	 }
-	     }
-	   
-	     
-	     
-	     public static void runJenkinsScript() throws IOException, InterruptedException{
-		 		Path directory = Paths.get(path);
-		 		
-		 		gitAllBranches(directory);
-	    	 
-		 		for(int i=0; i<branchesName.size();i++) {
-		 			String s=branchesName.get(i).replaceAll("[*\\s]+","");
-		 			
-		 			System.out.println("***************************");
-		 			System.out.println("="+s);
-		 			gitCeckout(directory, s);
-		 			gitPull(directory,s);
-		 			listAllFiles(path); // for rename Jenkins file
-		 			gitStage(directory); // git add .
-		 			gitCommit(directory, "change jenkins name "+i);
-		 			gitPush(directory,s);
-		 			
-		 		}
-	    	 
-	     }
-	     
+		String oldContent = "";
 
-	  // example of usage
-	 	private static void initAndAddFile() throws IOException, InterruptedException {
-	 		Path directory = Paths.get(path);
-	 		Files.createDirectories(directory);
-	 		gitInit(directory);
-	 		Files.write(directory.resolve("example.txt"), new byte[0]);
-	 		gitStage(directory);
-	 		gitCommit(directory, "Add example.txt");
-	 		
-	 	}
+		BufferedReader reader = null;
 
-	 	// example of usage
-	 	private static void cloneAndAddFile() throws IOException, InterruptedException {
-	 		String originUrl = "https://github.com/EslamElkhafagy/TestJenkinsFile.git";
-	 		Path directory = Paths.get(path);
-	 		gitClone(directory, originUrl);
-	 		Files.write(directory.resolve("example.txt"), new byte[0]);
-	 		gitStage(directory);
-	 		gitCommit(directory, "Add example.txt");
-//	 		gitPush(directory);
-	 	}
+		FileWriter writer = null;
 
-	 	public static void gitInit(Path directory) throws IOException, InterruptedException {
-	 		runCommand(directory, "git", "init");
-	 	}
+		try {
+			reader = new BufferedReader(new FileReader(fileToBeModified));
 
-	 	public static void gitStage(Path directory) throws IOException, InterruptedException {
-	 		runCommand(directory, "git", "add", ".");
-	 	}
+			// Reading all the lines of input text file into oldContent
 
-	 	public static void gitCommit(Path directory, String message) throws IOException, InterruptedException {
-	 		runCommand(directory, "git", "commit", "-m", message);
-	 	}
+			String line = reader.readLine();
 
-	 	public static void gitPush(Path directory,String branName) throws IOException, InterruptedException {
-	 		runCommand(directory, "git", "push","origin",branName);
-	 	}
-	 	public static void gitPull(Path directory,String branName) throws IOException, InterruptedException {
-	 		runCommand(directory, "git", "pull","origin",branName);
-	 	}
-	 	
-	 	public static void gitAllBranches(Path directory) throws IOException, InterruptedException {
-	 		current=1;
-	 		runCommand(directory, "git", "branch","-a");
-	 	}
-	
-	 	public static void gitCeckout(Path directory,String branchName) throws IOException, InterruptedException {
-	 		current=0;
-	 		
-	 		runCommand(directory, "git", "checkout",branchName);
+			while (line != null) {
+				
+				if (line.contains("${IMAGE_PREFIX}-")) {
+					System.out.println(line);
+					String[] arr = line.split("-");
+					String versions[] = arr[2].split("\\.");
+					String editBranch = branchVersion + "." + versions[versions.length-1];
+					String lineEdited = arr[0] + "-" + arr[1] + "-" + editBranch + "-" + arr[3];
+					System.out.println(lineEdited);
+					
+					oldContent = oldContent + lineEdited + System.lineSeparator();
+					
 
-	 	}
-
-	 	public static void gitClone(Path directory, String originUrl) throws IOException, InterruptedException {
-	 		runCommand(directory.getParent(), "git", "clone", originUrl, directory.getFileName().toString());
-	 	}
-
-	 	public static void runCommand(Path directory, String... command) throws IOException, InterruptedException {
-	 		Objects.requireNonNull(directory, "directory");
-	 		if (!Files.exists(directory)) {
-	 			throw new RuntimeException("can't run command in non-existing directory '" + directory + "'");
-	 		}
-	 		ProcessBuilder pb = new ProcessBuilder()
-	 				.command(command)
-	 				.directory(directory.toFile());
-	 		Process p = pb.start();
-	 		StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR");
-	 		StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT");
-	 		outputGobbler.start();
-	 		errorGobbler.start();
-	 		int exit = p.waitFor();
-	 		errorGobbler.join();
-	 		outputGobbler.join();
-	 		if (exit != 0) {
-	 			throw new AssertionError(String.format("runCommand returned %d", exit));
-	 		}
-	 	}
-	 	
-	 	private static class StreamGobbler extends Thread {
-
-			private final InputStream is;
-			private final String type;
-
-			private StreamGobbler(InputStream is, String type) {
-				this.is = is;
-				this.type = type;
-			}
-
-			@Override
-			public void run() {
-				try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
-					String line;
-					while ((line = br.readLine()) != null) {
-						System.out.println(type + "> " + line);
-						if(current==1)
-							branchesName.add(line);
-					}
-				} catch (IOException ioe) {
-					ioe.printStackTrace();
+				}else {
+					
+					oldContent = oldContent + line + System.lineSeparator();
+//					line = reader.readLine();	
 				}
+				line = reader.readLine();
+				
+			}
+			 System.out.println(oldContent);
+
+			// Replacing oldString with newString in the oldContent
+
+//			 String newContent = oldContent.replaceAll(oldString, lineEdited);
+
+			// Rewriting the input text file with newContent
+
+			writer = new FileWriter(fileToBeModified);
+
+			writer.write(oldContent);
+			// writer.write(newContent);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// Closing the resources
+
+				reader.close();
+
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
+	}
+
+	// init value was 6.0.1-SNAPSHOT
+
+	public static final String xmlFilePath = "/media/elkhafagy/Elkhafagy/Elkhafagy/special/VFES-DXL-Fault-Management/pom.xml";
+
+	public static void editPomXMl(String commonVersion) {
+
+		try {
+
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+			Document document = documentBuilder.parse(xmlFilePath);
+
+			// Get employee by tag name
+			// use item(0) to get the first node with tage name "employee"
+			Node employee = document.getElementsByTagName("properties").item(0);
+
+			// update employee , set the id to 10
+			NamedNodeMap attr = employee.getAttributes();
+			Node nodeAttr = attr.getNamedItem("common.version");
+
+			// loop the employee node and update salary value, and delete a node
+			NodeList nodes = employee.getChildNodes();
+
+			for (int i = 0; i < nodes.getLength(); i++) {
+
+				Node element = nodes.item(i);
+
+				if ("common.version".equals(element.getNodeName())) {
+					System.out.println("*********************************");
+					System.out.println(element.getTextContent());
+					String arr[] = element.getTextContent().split("-");
+					String newVersion = commonVersion + "-" + arr[1];
+					element.setTextContent(newVersion);
+					System.out.println("********************************");
+				}
+
+			}
+
+			// write the DOM object to the file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource domSource = new DOMSource(document);
+
+			StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+			transformer.transform(domSource, streamResult);
+
+			System.out.println("The XML File was ");
+
+			// write the content on console
+			//
+			DOMSource source = new DOMSource(document);
+			System.out.println("-----------Modified File-----------");
+			StreamResult consoleResult = new StreamResult(System.out);
+			transformer.transform(source, consoleResult);
+
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (SAXException sae) {
+			sae.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		// runJenkinsScript();
+
+		// editPomXMl("9.0.0.0");
+		modifyFile(jenkinsPath, "6.5.7.0.0.9");
+
+	}
+
+	// Uses Files.walk method
+	public static void listAllFiles(String path) {
+		System.out.println("In listAllfiles(String path) method");
+		try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+			paths.forEach(filePath -> {
+				if (Files.isRegularFile(filePath)) {
+					try {
+						files.add(new File(filePath.toString()));
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+			renameJenkinsFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void renameJenkinsFile() {
+		int count = 0;
+		for (int i = 0; i < files.size(); i++) {
+			if (files.get(i).getName().startsWith("jenkinsfile"))
+				files.get(i).renameTo(new File(files.get(i).getPath() + "1"));
+			// System.out.println(files.get(i).getPath());
+		}
+	}
+
+	public static void runJenkinsScript() throws IOException, InterruptedException {
+		Path directory = Paths.get(path);
+
+		gitAllBranches(directory);
+
+		for (int i = 0; i < branchesName.size(); i++) {
+			String s = branchesName.get(i).replaceAll("[*\\s]+", "");
+
+			System.out.println("***************************");
+			System.out.println("=" + s);
+			gitCeckout(directory, s);
+			gitPull(directory, s);
+			listAllFiles(path); // for rename Jenkins file
+			gitStage(directory); // git add .
+			gitCommit(directory, "change jenkins name " + i);
+			gitPush(directory, s);
+
+		}
+
+	}
+
+	// example of usage
+	private static void initAndAddFile() throws IOException, InterruptedException {
+		Path directory = Paths.get(path);
+		Files.createDirectories(directory);
+		gitInit(directory);
+		Files.write(directory.resolve("example.txt"), new byte[0]);
+		gitStage(directory);
+		gitCommit(directory, "Add example.txt");
+
+	}
+
+	// example of usage
+	private static void cloneAndAddFile() throws IOException, InterruptedException {
+		String originUrl = "https://github.com/EslamElkhafagy/TestJenkinsFile.git";
+		Path directory = Paths.get(path);
+		gitClone(directory, originUrl);
+		Files.write(directory.resolve("example.txt"), new byte[0]);
+		gitStage(directory);
+		gitCommit(directory, "Add example.txt");
+		// gitPush(directory);
+	}
+
+	public static void gitInit(Path directory) throws IOException, InterruptedException {
+		runCommand(directory, "git", "init");
+	}
+
+	public static void gitStage(Path directory) throws IOException, InterruptedException {
+		runCommand(directory, "git", "add", ".");
+	}
+
+	public static void gitCommit(Path directory, String message) throws IOException, InterruptedException {
+		runCommand(directory, "git", "commit", "-m", message);
+	}
+
+	public static void gitPush(Path directory, String branName) throws IOException, InterruptedException {
+		runCommand(directory, "git", "push", "origin", branName);
+	}
+
+	public static void gitPull(Path directory, String branName) throws IOException, InterruptedException {
+		runCommand(directory, "git", "pull", "origin", branName);
+	}
+
+	public static void gitAllBranches(Path directory) throws IOException, InterruptedException {
+		current = 1;
+		runCommand(directory, "git", "branch", "-a");
+	}
+
+	public static void gitCeckout(Path directory, String branchName) throws IOException, InterruptedException {
+		current = 0;
+
+		runCommand(directory, "git", "checkout", branchName);
+
+	}
+
+	public static void gitClone(Path directory, String originUrl) throws IOException, InterruptedException {
+		runCommand(directory.getParent(), "git", "clone", originUrl, directory.getFileName().toString());
+	}
+
+	public static void runCommand(Path directory, String... command) throws IOException, InterruptedException {
+		Objects.requireNonNull(directory, "directory");
+		if (!Files.exists(directory)) {
+			throw new RuntimeException("can't run command in non-existing directory '" + directory + "'");
+		}
+		ProcessBuilder pb = new ProcessBuilder().command(command).directory(directory.toFile());
+		Process p = pb.start();
+		StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR");
+		StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT");
+		outputGobbler.start();
+		errorGobbler.start();
+		int exit = p.waitFor();
+		errorGobbler.join();
+		outputGobbler.join();
+		if (exit != 0) {
+			throw new AssertionError(String.format("runCommand returned %d", exit));
+		}
+	}
+
+	private static class StreamGobbler extends Thread {
+
+		private final InputStream is;
+		private final String type;
+
+		private StreamGobbler(InputStream is, String type) {
+			this.is = is;
+			this.type = type;
+		}
+
+		@Override
+		public void run() {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					System.out.println(type + "> " + line);
+					if (current == 1)
+						branchesName.add(line);
+				}
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+	}
 
 }
